92f9472922bbab295e4fae0d9d4f2b4a5d4a7828
MDJAmin <mohammadamindf@gmail.com>
2024-09-27 18:24:58 +0330

    Refactor : Update JS
     import Dom items and create songs object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict"
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+
